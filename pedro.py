7f9fdafda38d3b608b9805cdcc6a7d5f168d6442
Pedro De Oliveira Silva <pedrooliveirasilva@professor.educacao.sp.gov.br>
2024-06-18 18:52:50 -0300

    first commit

---
+++ pedro.py
@@ -0,0 +1,14 @@
+nome = input ("digite seu nome ")
+idade = int(input("digite sua idade"))
+
+if idade == 17:
+    print(" jogara no sub_17")
+    
+elif idade == 20:
+    print("jogara no sub_20")
+
+elif  idade == 21 or 25:
+    print("voce jogara profissionalmente ")
+
+else:
+    print("voce não podera entrar na competição")
